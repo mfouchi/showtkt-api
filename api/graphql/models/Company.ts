18f061d5f5da55ce54f9770e052fd16c18e69527
Marc Fouchi <mfouchi@gmail.com>
2020-07-29 20:48:19 -0500

    updated company schema

--- api/graphql/models/Company.ts
+++ api/graphql/models/Company.ts
@@ -1,22 +1,24 @@
-import { schema } from 'nexus'
+import { schema } from 'nexus';
 
 schema.objectType({
   name: 'Company',
   definition(t) {
-    t.model.id()
-    t.model.name()
-    t.model.Producers()
-    t.model.Productions()
-    t.model.Events()
-    t.model.Venues()
+    t.model.id();
+    t.model.name();
+    t.model.city();
+    t.model.state();
+    t.model.Producers();
+    t.model.Productions();
+    t.model.Events();
+    t.model.Venues();
   },
-})
+});
 
 schema.extendType({
   type: 'Query',
   definition(t) {
-    t.crud.company()
-    t.crud.companies({ filtering: true, ordering: true })
+    t.crud.company();
+    t.crud.companies({ filtering: true, ordering: true });
 
     t.field('companiesCount', {
       type: 'Int',
@@ -24,21 +26,21 @@ schema.extendType({
         where: 'CompanyWhereInput',
       },
       async resolve(_root, args, ctx) {
-        return ctx.db.company.count(args)
+        return ctx.db.company.count(args);
       },
-    })
+    });
   },
-})
+});
 
 schema.extendType({
   type: 'Mutation',
   definition(t) {
-    t.crud.createOneCompany()
-    t.crud.updateOneCompany()
-    t.crud.upsertOneCompany()
-    t.crud.deleteOneCompany()
+    t.crud.createOneCompany();
+    t.crud.updateOneCompany();
+    t.crud.upsertOneCompany();
+    t.crud.deleteOneCompany();
 
-    t.crud.updateManyCompany()
-    t.crud.deleteManyCompany()
+    t.crud.updateManyCompany();
+    t.crud.deleteManyCompany();
   },
-})
+});
